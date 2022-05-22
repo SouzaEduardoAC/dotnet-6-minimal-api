98f409e75b964687d7aaa39c9a582082c5b23290
Eduardo Augusto Coleta de Souza <souzaeduardoac@gmail.com>
2022-05-22 19:35:26 -0300

    Some small changes

--- StandardApi/Program.cs
+++ StandardApi/Program.cs
@@ -4,13 +4,13 @@ using DotNetCore6Api.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
